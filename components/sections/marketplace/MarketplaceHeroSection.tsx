c48b65d37934e25e473d892cb9d082bbccdfb8e2
SyariefHayat <muhammadsyarifhayat@gmail.com>
2026-04-15 11:56:36 +0700

    feat(marketplace): setup page structure and metadata for issue #71

---
+++ components/sections/marketplace/MarketplaceHeroSection.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MarketplaceHeroSection = () => {
+  return (
+    <section>
+      <div className='container-smarttani py-10'>
+        <h2 className='text-heading-2'>MarketplaceHeroSection Placeholder</h2>
+      </div>
+    </section>
+  );
+};
+
+export default MarketplaceHeroSection;
---
+++ components/sections/marketplace/TrustBarSection.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TrustBarSection = () => {
+  return (
+    <section>
+      <div className='container-smarttani py-10'>
+        <h2 className='text-heading-2'>TrustBarSection Placeholder</h2>
+      </div>
+    </section>
+  );
+};
+
+export default TrustBarSection;
